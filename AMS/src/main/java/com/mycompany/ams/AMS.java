838a4b4f046f858d152e527be0e730d36d4d28a8
AianLouise <89790869+AianLouise@users.noreply.github.com>
2022-11-30 06:50:26 +0800

    Add files via upload

---
+++ AMS/src/main/java/com/mycompany/ams/AMS.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ams;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aianl
+ */
+public class AMS {
+    
+//    public static Connection connnectmysqldb(){
+//        try{
+//            String username = "root";
+//            String pass = "Aianlouise24@";
+//            String sqlconn = "jdbc:mysql://localhost:3306/ams";
+//            
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection conn = DriverManager.getConnection(sqlconn, username, pass);
+//            return conn;
+//            
+//        }
+//        catch(ClassNotFoundException | SQLException e){
+//            JOptionPane.showMessageDialog(null,e);
+//        }
+//        return null;
+//    }
+    
+    public static Connection connectmysqldb() {
+        try{
+            String username = "root";
+            String pass = "Aianlouise24@";
+            String sqlconn = "jdbc:mysql://localhost:3306/ams";
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(sqlconn, username, pass);
+            return conn;
+            
+        }
+        catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    
+}
